980f0e02c99fdcee7edbbce0f0f1c0bd19aa70c0
Renan Assis <renan.assis.alves@hotmail.com>
2020-11-19 16:36:46 -0300

    Initial

--- Projeto1/index.php
+++ Projeto1/index.php
@@ -1,5 +1,4 @@
 <?php
 
-echo nl2br("Versao php: " . phpversion() . "\n Cookie:" .$_COOKIE[1] . "\n Env: " . $_ENV);
-
-echo "<h1>Talkei tavinho</h1>";
+$teste = [1, 2, 3];
+echo "<h1>Numeros = $teste</h1>";
