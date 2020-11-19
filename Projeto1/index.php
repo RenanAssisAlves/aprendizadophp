3f51a2a2d42b487f97bd75b9370ac85ece0c62ca
Renan Assis <renan.assis.alves@hotmail.com>
2020-11-19 15:16:49 -0300

    initial version

--- Projeto1/index.php
+++ Projeto1/index.php
@@ -1,7 +1,5 @@
 <?php
 
-echo "Versao php: " . phpversion() . "\n Cookie:" .$_COOKIE . "\n Env: " . $_ENV;
-echo($_COOKIE);
-echo($_ENV);
+echo nl2br("Versao php: " . phpversion() . "\n Cookie:" .$_COOKIE . "\n Env: " . $_ENV);
 
 echo "<h1>Talkey</h1>";
