a41104289bb38f4a9ac05a97a322dac589b0f30b
Renan Assis <renan.assis.alves@hotmail.com>
2020-11-24 15:23:41 -0300

    initial version

---
+++ Projeto1/form_insert.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Insert em PhP</title>
+    </head>
+
+    <body>
+        <form action="insert.php" method="POST">
+            <input type="text" name="nome" placeholder="Digite o seu nome...">
+            <input type="email" name="email" placeholder="Digite o seu email...">
+            <input type="password" name="senha" placeholder="Digite a sua senha...">
+            <input type="submit" value="Cadastrar">
+        </form>
+    </body>
+</html>
+<?php
+
+
--- Projeto1/insert.php
+++ Projeto1/insert.php
@@ -1,9 +1,11 @@
 <?php
 include "connect.php";
 
-$nome = "Fábio Venâncio";
-$email = "fabio@gmail.com";
-$senha = "fabio123";
+$nome = $_POST('nome');
+$email = $_POST('email');
+$senha = $_POST('senha');
 
 mysqli_query($link, "insert into login(nome, email, senha) 
 values('$nome','$email','$senha')");
+
+header('location:form_insert.php');
