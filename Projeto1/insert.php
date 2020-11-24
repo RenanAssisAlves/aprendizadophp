9797919137775d6cac29858d7b432e3c1d40cff3
Renan Assis <renan.assis.alves@hotmail.com>
2020-11-24 15:36:37 -0300

    initial version

--- Projeto1/insert.php
+++ Projeto1/insert.php
@@ -1,9 +1,9 @@
 <?php
 include "connect.php";
 
-$nome = $_POST('nome');
-$email = $_POST('email');
-$senha = $_POST('senha');
+$nome = 'nome';
+$email = 'email';
+$senha = 'senha';
 
 mysqli_query($link, "insert into login(nome, email, senha) 
 values('$nome','$email','$senha')");
