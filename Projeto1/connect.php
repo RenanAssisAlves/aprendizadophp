fb88274c39ec24f1f262d9a193d671021aec45f4
Renan Assis <renan.assis.alves@hotmail.com>
2020-11-24 14:30:53 -0300

    initial version

---
+++ Projeto1/connect.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$user = "u738809170_renan";
+$pass = "449999Renan";
+$db = "u738809170_crud";
+
+$link = mysqli_connect($host, $user, $pass, $db);
--- Projeto1/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$teste = [1, 2, 3];
-echo "<h1>Numeros = $teste</h1>";
