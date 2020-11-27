fc4426b70fbc420c4a5d6c5704deba6fedb1a376
RenanAssisAlves <renan.assis.alves@hotmail.com>
2020-11-27 01:11:38 -0300

    Initial release

--- Projeto2/cadastrar.php
+++ Projeto2/cadastrar.php
@@ -87,7 +87,7 @@
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
             </div>
-            <button type="submit" class="btn btn-primary">Enviar</button>
+            <button type="submit" class="btn btn-primary" style="float:right">Enviar</button>
         </form>
 
     </div>
