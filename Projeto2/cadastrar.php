a134648257ff398aded421bc701a4f9f55181a16
RenanAssisAlves <renan.assis.alves@hotmail.com>
2020-11-27 00:59:02 -0300

    Initial release

--- Projeto2/cadastrar.php
+++ Projeto2/cadastrar.php
@@ -77,21 +77,17 @@
 <main role="main" class="container">
 
     <div class="starter-template">
-        <form class=".col-sm-5.">
-            <div class="form-group">
+        <form>
+            <div class="form-group .col-sm-5.">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
-            <div class="form-group">
+            <div class="form-group .col-sm-5.">
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
             </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
 
     </div>
