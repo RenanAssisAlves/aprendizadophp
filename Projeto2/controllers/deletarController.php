deb9ddc18d4cfcc6aa38bda26120aa29c2ab5fbe
RenanAssisAlves <renan.assis.alves@hotmail.com>
2020-11-30 04:46:10 -0300

    Initial release

--- Projeto2/controllers/deletarController.php
+++ Projeto2/controllers/deletarController.php
@@ -3,6 +3,6 @@ include_once "connect.php";
 
 $iddeletar = addslashes($_POST['id']);
 
-mysqli_query($link, "delete from cliente where id = '$iddeletar'");
+mysqli_query($link, "delete from cliente where cliente_id = '$iddeletar'");
 
 header('location:../deletar.php');
