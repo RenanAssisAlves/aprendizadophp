245085aec1a1bb9f80d660f93756a43d48184bc8
RenanAssisAlves <renan.assis.alves@hotmail.com>
2020-11-30 04:43:48 -0300

    Initial release

--- Projeto2/atualizar.php
+++ Projeto2/atualizar.php
@@ -132,11 +132,11 @@ $cad = $_GET['cad'];
         {
             ?>
             <p> Qual cadastro deseja alterar?</p>
-            <form method="POST" action="?cad=1&exibir=0">
+            <form method="POST" action="?cad=1&exibir=1">
                 <input class="btn btn-primary" type="submit" value="Cliente">
             </form>
             <br>
-            <form method="POST" action="?cad=2&exibir=0">
+            <form method="POST" action="?cad=2&exibir=1">
                 <input class="btn btn-primary" type="submit" value="Endereco">
             </form>
         <?
