de68a88a84aa5e4490ee2b80296383d2d800a52a
RenanAssisAlves <renan.assis.alves@hotmail.com>
2020-11-30 02:24:20 -0300

    Initial release

--- Projeto2/controllers/cadastrarController.php
+++ Projeto2/controllers/cadastrarController.php
@@ -1,7 +1,7 @@
 <?php
 include "connect.php";
 
-$nome = $_POST['nome'];
+$nome = addslashes($_POST['nome']);
 $email = addslashes($_POST['email']);
 $senha = addslashes($_POST['senha']);
 $rua = addslashes($_POST['rua']);
@@ -10,18 +10,18 @@ $bairro = addslashes($_POST['bairro']);
 $cidade = addslashes($_POST['cidade']);
 $cep = addslashes($_POST['cep']);
 
-echo("o nome é ". $nome);
 mysqli_query($link, "insert into cidade(nome)
 values('$cidade')");
 
-$idcidade = mysqli_query($link,"select cidade_id from cidade where cidade.nome = '$nome')");
+$result = mysqli_query($link,"select cidade_id from cidade where cidade.nome = '$nome')");
+$row=$result->fetch_object();
 
-$cidadequery = mysqli_fetch_field($idcidade)->cidade_id;
+$idendereco = $row->cidade_id;
+
 mysqli_query($link, "insert into endereco(rua, bairro, cidade_id, cep)
- values('$rua', '$bairro', '$idcidade', '$cep'");
+ values('$rua', '$bairro', '$idcidade', '$cep')");
 
 $idendereco = mysqli_insert_id();
 mysqli_query($link, "insert into cliente(nome, email, senha, entrega_endereco_id, entrega_numero) 
 values('$nome','$email','$senha', '$idendereco', '$numero')");
-
 header('location:../cadastrar.php');
