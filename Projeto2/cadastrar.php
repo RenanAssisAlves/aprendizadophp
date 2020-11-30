91e6b203118dadd3d0746b3186e51c028efbaf36
RenanAssisAlves <renan.assis.alves@hotmail.com>
2020-11-30 01:54:56 -0300

    Initial release

--- Projeto2/cadastrar.php
+++ Projeto2/cadastrar.php
@@ -81,22 +81,22 @@
             <div class="form-group">
                 <small class="form-text text-muted">Realize o seu cadastro</small>
                 <label>Nome</label>
-                <input type="text" class="form-control" nome="nome" placeholder="Digite o seu nome">
+                <input type="text" class="form-control" name="nome" placeholder="Digite o seu nome">
                 <label>Email</label>
-                <input type="email" class="form-control" nome="email" placeholder="Digite o seu email">
+                <input type="email" class="form-control" name="email" placeholder="Digite o seu email">
                 <label>Senha</label>
-                <input type="password" class="form-control" nome="senha" placeholder="Digite a sua senha">
+                <input type="password" class="form-control" name="senha" placeholder="Digite a sua senha">
                 <small class="form-text text-muted">Endereço de entrega</small>
                 <label>Rua</label>
-                <input type="text" class="form-control" nome="rua" placeholder="Digite a Rua">
+                <input type="text" class="form-control" name="rua" placeholder="Digite a Rua">
                 <label>Número</label>
-                <input type="text" class="form-control" nome="numero" placeholder="Digite o número da casa">
+                <input type="text" class="form-control" name="numero" placeholder="Digite o número da casa">
                 <label>Bairro</label>
-                <input type="text" class="form-control" nome="bairro" placeholder="Digite o Bairro">
+                <input type="text" class="form-control" name="bairro" placeholder="Digite o Bairro">
                 <label>Cidade</label>
-                <input type="text" class="form-control" nome="cidade" placeholder="Digite a Cidade">
+                <input type="text" class="form-control" name="cidade" placeholder="Digite a Cidade">
                 <label>CEP</label>
-                <input type="text" class="form-control" nome="cep" placeholder="Digite o CEP">
+                <input type="text" class="form-control" name="cep" placeholder="Digite o CEP">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
