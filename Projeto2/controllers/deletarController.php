03660bf7e143b2772f566d2ac8e7e1a123a79bb1
Renan Assis <renan.assis.alves@hotmail.com>
2020-12-02 17:31:03 -0300

    initial version

--- Projeto2/controllers/deletarController.php
+++ Projeto2/controllers/deletarController.php
@@ -8,12 +8,12 @@ if ($deletar == 0)
     {
         mysqli_query($link, "delete from cliente where cliente_id = '$iddeletar'");
 
-        header("Refresh");
+        header('location:../visualizar.php');
     }
 
 else if($deletar == 1)
 {
 mysqli_query($link, "delete from endereco where endereco_id = '$iddeletar'");
 
-header("Refresh");
+    header('location:../visualizar.php');
 }
